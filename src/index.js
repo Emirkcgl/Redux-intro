2583dae30c2cd79fcd273d2590911b4df4c064f2
Emirkcgl <yunus0598koca@gmail.com>
2026-08-05 10:49:46 +0300

    Redux File Structure: State Slices

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import store from "./store";
 
+store.dispatch({ type: "account/deposit", payload: 1000 });
+console.log(store.getState());
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
